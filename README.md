e0d0adcabab4fcdf95327d0c6549e25053d7d943
AndrewMurtagh <22800080+AndrewMurtagh@users.noreply.github.com>
2025-01-17 23:05:22 +0000

    Miscellaneous changes for setting up repo

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Open source OTA updates.
 
+## About
+
+A web-based platform for managing OTA updates focused on simplicity, ease of use, and self-hosting.
+
+Uses Hawkbit DDI API as a wire format so is compatible with RAUC and SWUpdate.
+
+Very much in Alpha.
+
 ## License
 
 [AGPL-3.0](LICENSE)
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet';
 import type { NestExpressApplication } from '@nestjs/platform-express'
-import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     disableErrorMessages: app.get(ConfigService).get('NODE_ENV') === 'production',
     whitelist: true,
