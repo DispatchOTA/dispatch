6b28d83814c9eaa5c9b6dd0fad5e82761fa98d99
AndrewMurtagh <22800080+AndrewMurtagh@users.noreply.github.com>
2025-01-26 22:28:53 +0000

    Adding image versions

--- apps/frontend/src/images/detail.tsx
+++ apps/frontend/src/images/detail.tsx
@@ -7,7 +7,8 @@ import { ErrorState } from '../components/AsyncUtils';
 import { DateTime } from '../components/DateTime';
 import { DetailView } from '../components/DetailView';
 import { DetailField } from '../components/DetailView';
-import { Image } from '../../types';
+import { Image, ImageVersion } from '../../types';
+import { AsyncTable, AsyncTableColumn } from '../components/AsyncTable';
 
 const imageDetailFields: DetailField<Image>[] = [
   {
@@ -28,6 +29,21 @@ const imageDetailFields: DetailField<Image>[] = [
   },
 ];
 
+const imageVersionColumns: AsyncTableColumn<ImageVersion>[] = [
+  {
+    header: 'ID',
+    accessor: 'id'
+  },
+  {
+    header: 'Description',
+    accessor: 'description'
+  },
+  {
+    header: 'Created At',
+    accessor: (imageVersion: ImageVersion) => <DateTime date={imageVersion.createdAt} />
+  }
+];
+
 const ImageDetail = () => {
   const { id } = useParams();
 
@@ -43,6 +59,15 @@ const ImageDetail = () => {
             endpoint='/images'
             queryKey='image'
             fields={imageDetailFields}
+          />
+        </Card>
+        <Card title='Versions'>
+          <AsyncTable<ImageVersion>
+            queryKey={`image-${id}-versions`}
+            endpoint={`/images/${id}/versions`}
+            columns={imageVersionColumns}
+            emptyMessage='No versions found'
+            errorMessage='Error loading versions'
           />
         </Card>
       </Column>
